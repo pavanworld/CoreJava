07790e1a3fedf5808a881172dc8cc6316ee9dd92
pavan <pavanjanapati1216@gmail.com>
2020-11-18 15:43:52 +0530

    Student class method is checked

--- GitProject/src/com/nt/entity/Student.java
+++ GitProject/src/com/nt/entity/Student.java
@@ -1,5 +1,14 @@
 package com.nt.entity;
 
+
 public class Student {
+	
+	public Float calculateStudentPercentage(Float[] marks) {
+		float sum=0;
+		for(float sub:marks) {
+			sum=sum+sub;
+		}
+		return (sum/6);
+	}
 
 }
--- GitProject/src/com/nt/test/AvergaeTest.java
+++ GitProject/src/com/nt/test/AvergaeTest.java
@@ -1,9 +1,11 @@
 package com.nt.test;
 
+import com.nt.entity.Student;
+
 public class AvergaeTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Your Percentage is::"+new Student().calculateStudentPercentage(new Float[] {70f,70f,70f,80f,80f,80f}));
 
 	}
 
